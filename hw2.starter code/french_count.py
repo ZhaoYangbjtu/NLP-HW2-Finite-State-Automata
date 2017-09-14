7aa4d14cdf59901282629fced04f520e81077e72
Sanjay Nadhavajhala <sanjay920@me.com>
2017-09-14 02:25:33 -0700

    french numbers 0-69 are working

--- hw2.starter code/french_count.py
+++ hw2.starter code/french_count.py
@@ -18,28 +18,57 @@ def prepare_input(integer):
       "Integer out of bounds"
     return list("%03i" % integer)
 
-prepare_input(996)
+# prepare_input(996)
 
 def french_count():
     f = FST('french')
 
     f.add_state('start')
-    f.add_state('0XX')
-    f.add_state('00X')
+    f.add_state('XX')
+    f.add_state('X')
+    f.add_state('1X')
+
     # final state for single digit numbers
-    f.add_state('00N')
     f.add_state('final')
 
 
     f.initial_state = 'start'
 
-    f.set_final('00N')
     f.set_final('final')
 
     # f.add_arc('start', '0XX', [str(ii)], [kFRENCH_TRANS[ii]])
 
     for key in kFRENCH_TRANS.keys():
         f.add_arc('start', 'final', prepare_input(key), [kFRENCH_TRANS[key]])
+
+    for ii in xrange(10):
+        if ii == 0:
+            f.add_arc('start', 'XX', ['0'], ())
+        else:
+            if ii == 1:
+                f.add_arc('start', 'XX', [str(ii)], ['cent'])
+                f.add_arc('X', 'final', [str(ii)], ['et',kFRENCH_TRANS[ii]])
+            else:
+                f.add_arc('start', 'XX', [str(ii)], [kFRENCH_TRANS[ii], 'cent'])
+                f.add_arc('X', 'final', [str(ii)], [kFRENCH_TRANS[ii]])
+
+    for ii in xrange(2,7):
+        f.add_arc('XX', 'X', [str(ii)], [kFRENCH_TRANS[ii*10]])
+
+    f.add_arc('XX', '1X', ['1'], ['dix'])
+    f.add_arc('1X', 'final', ['7'], [kFRENCH_TRANS[7]])
+    f.add_arc('1X', 'final', ['8'], [kFRENCH_TRANS[8]])
+    f.add_arc('1X', 'final', ['9'], [kFRENCH_TRANS[9]])
+
+
+    # This arc isn't necessary as it is implicitly there by from adding the keys
+    # from kFRENCH_TRANS...e.g. 1 --> ['0','0','1'] is the input symbol from
+    # 'start' to 'final'
+    # f.add_arc('XX', 'X', ['0'], ())
+
+    f.add_arc('X', 'final', [str(ii)], [kFRENCH_TRANS[ii]])
+
+
 
     # for ii in xrange(10):
     #     if ii == 0:
@@ -49,19 +78,21 @@ def french_count():
 
 
     return f
-
-french_count().transduce(prepare_input(996))
-trace(french_count(),prepare_input(996))
-graphviz_writer(french_count(),'french_count.dot')
+#
+# french_count().transduce(prepare_input(19))
+# trace(french_count(),prepare_input(996))
+# graphviz_writer(french_count(),'french_count.dot')
 
 
 if __name__ == '__main__':
-    string_input = raw_input()
-    user_input = int(string_input)
+    # string_input = raw_input()
+    # user_input = int(string_input)
     f = french_count()
-    if string_input:
-        print user_input, '-->',
-        print " ".join(f.transduce(prepare_input(user_input)))
+    # if string_input:
+    #     print user_input, '-->',
+    #     print " ".join(f.transduce(prepare_input(user_input)))
+    for i in range(100):
+        print i, " ".join(f.transduce(prepare_input(i)))
 
 
 # def graphviz_writer(fst,fname):
